839a6e3206da2a3869b4e85a66f48b7dfdcd24a1
dmart126 <dmart126@DESKTOP-HMA8JE5>
2023-10-20 14:07:07 +0200

    Add project files.

---
+++ DAMComponentLibrary/TableControl.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DAMComponentLibrary
+{
+    /// <summary>
+    /// Interaction logic for TableControl.xaml
+    /// </summary>
+    public partial class TableControl : UserControl
+    {
+        
+        private int _rows = 2;
+        private int _cols = 2;
+        public TableControl()
+        {
+            InitializeComponent();
+            InitTable();
+        }
+
+        private void InitTable()
+        {
+            mainGrid.ColumnDefinitions.Clear();
+            mainGrid.RowDefinitions.Clear();
+            mainGrid.Children.Clear();
+
+            for (int c = 0; c < Cols; c++)
+            {
+                mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int r = 0; r < Rows; r++)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int c = 0; c < Cols; c++)
+            {
+                for (int r = 0; r < Rows; r++)
+                {
+                    Label l = new Label();                   
+                    l.Content = "Lbl(" + r + "," + c + ")";
+                    
+                    Grid.SetRow(l, r);
+                    Grid.SetColumn(l, c);
+
+                    mainGrid.Children.Add(l);
+                }
+            }
+        
+        }
+
+        public int Rows
+        {
+            get
+            {
+                return _rows;
+            }
+            set
+            {
+                _rows = value;
+                InitTable();
+            }
+        } 
+
+        public int Cols
+        {
+            get
+            {
+                return _cols;
+            }
+            set
+            {
+                _cols = value;
+                InitTable();
+            }
+        }
+    }
+}
